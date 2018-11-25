284507926c3a53d8551841ae2f459d80fec7e5b9
Steven Ott <stevenkott@icloud.com>
2018-11-25 23:53:46 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -52,8 +52,8 @@ def monopoly_with_third_tier
 	      :shortline => {
 	        
 	      }
-	      }
-	    },
+	      },
+	    
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
