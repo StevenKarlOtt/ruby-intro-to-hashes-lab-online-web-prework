c8883d3f34d0e510b56c2289b82db8e220fd1a39
Steven Ott <stevenkott@icloud.com>
2018-11-25 23:51:47 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {
 	      },
-	      :pennsylvannia_railroad => {
+	      :pennsylvania_railroad => {
 	        
 	      }
 	    },
